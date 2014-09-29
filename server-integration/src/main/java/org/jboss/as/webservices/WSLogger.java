92aa132dbb76bfd22e362dd9c136ca58183ee6d7
Alessio Soldano <asoldano@users.noreply.github.com>
2014-09-29 09:22:03 +0000

    Backport [WFLY-3535] check

--- server-integration/src/main/java/org/jboss/as/webservices/WSLogger.java
+++ server-integration/src/main/java/org/jboss/as/webservices/WSLogger.java
@@ -300,4 +300,7 @@ public interface WSLogger extends BasicLogger {
             + WS_SPEC_REF_5_3_2_4_2)
     void finalizeMethodNotAllowed(Class<?> seiClass);
 
+    @LogMessage(level = WARN)
+    @Message(id = 15617, value = "Annotation '@%s' found on class '%s'. Perhaps you forgot to add a '%s' module dependency to your deployment?")
+    void missingModuleDependency(String annotation, String clazz, String module);
 }
